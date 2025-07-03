0a9a78e0307188447ba2a3ce119495c0881a00f4
Ohene Gyan <ohenegyan@Ohenes-MacBook-Pro.local>
2025-07-03 20:50:57 +0000

    new

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
